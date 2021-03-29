2c547b7ddc992eaf3f6fb39d5861aed04c5650d1
valter-tonon <tononvalter@gmail.com>
2021-03-28 21:20:20 -0300

    add header

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: process.env.REACT_APP_API_START
+    baseURL: 'http://129.146.170.175/api/'/*process.env.REACT_APP_API_START*/
 })
